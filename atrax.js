3049d3253f947e78ce50918383ce67a7e1140dc1
mashiya <farbod.motlagh@gmail.com>
2013-07-25 18:52:04 +0930

    Change Indentation

--- atrax.js
+++ atrax.js
@@ -13,7 +13,7 @@ var http = require('http')
  * Inherit from EventEmitter
  */
 function EventClass(){
-	events.EventEmitter.call(this);
+  events.EventEmitter.call(this);
 }
 util.inherits(EventClass, events.EventEmitter);
 var atrax = new EventClass();
@@ -26,24 +26,24 @@ atrax.options = {
 };
 
 atrax.crawl = function(url, re, callback) {
-	if (typeof url === 'object') {
-		url = urlLib.format(url);
-	} else {
-		if (!/((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(url)){
-			console.error('\033[34m atrax \033[0m - URL is not valid');
-			return false;
-		}		
-	}
-	http.get(url, function(res){
-		var html = '';
-		res.setEncoding('utf8');
-		res.on('data', function(data){
-			html += data;
-		});
-		res.on('end', function(){
-			callback(re.exec(html));
-		});
-	});
+  if (typeof url === 'object') {
+    url = urlLib.format(url);
+  } else {
+    if (!/((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(url)){
+      console.error('\033[34m atrax \033[0m - URL is not valid');
+      return false;
+    }
+  }
+  http.get(url, function(res){
+    var html = '';
+    res.setEncoding('utf8');
+    res.on('data', function(data){
+      html += data;
+    });
+    res.on('end', function(){
+      callback(re.exec(html));
+    });
+  });
 };
 
 module.exports = atrax;
--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var util = require('util');
 var atrax = require('../atrax');
 var re = /<td><strong>Gold<\/strong><\/td>\s+<td>(\S+)<\/td>\s+<td>(\S+)<\/td>/;
 atrax.crawl('http://www.usagold.com/mobile/',re,function(result){
-    console.log(result[1]);
-	console.log(result[2]);
+  console.log(result[1]);
+  console.log(result[2]);
 });
