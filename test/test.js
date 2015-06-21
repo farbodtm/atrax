1ad1fa7e029b545a935623c821fc575bd0899f1d
Farbod T.Motlagh <farbod.motlagh@gmail.com>
2015-06-21 23:23:43 +0930

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,17 +1,24 @@
+// Atrax test
+
 var util = require('util');
 var atrax = require('../atrax');
+
 var url = 'http://www.usagold.com/mobile/';
 var re = /<td><strong>Gold<\/strong><\/td>\s+<td>(\S+)<\/td>\s+<td>(\S+)<\/td>/;
+
 atrax.crawl(url, re, function(result){
   console.log(result[1]);
   console.log(result[2]);
 });
+
 atrax.getTitle(url, function(result){
   console.log(result);
 });
+
 atrax.getLinks(url, function(result){
   console.log(result);
 });
+
 atrax.getDensity(url, 'gold', true, function(result){
   console.log(result);
 });
